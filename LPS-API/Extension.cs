4dde291aad19e3ab05cf7e25fafe4743707215e1
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-05-27 23:36:29 +0900

    feat: 확장 메서드 AddWhereClause() 구현

---
+++ LPS-API/Extension.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace LPS_API
+{
+    public static class Extension
+    {
+        public static void AddWhereClause(this MySqlCommand cmd, Dictionary<string, object> args)
+        {
+            foreach (KeyValuePair<string, object> arg in args)
+            {
+                if (arg.Value == null)
+                {
+                    args.Remove(arg.Key);
+                }
+            }
+
+            if (args.Count > 0)
+            {
+                cmd.CommandText += " WHERE ";
+                int index = 0;
+
+                foreach (KeyValuePair<string, object> arg in args)
+                {
+                    index++;
+
+                    cmd.CommandText += string.Format("{0} = @{0}", arg.Key);
+                    cmd.Parameters.AddWithValue("@" + arg.Key, arg.Value);
+
+                    if (index < args.Count)
+                    {
+                        cmd.CommandText += " AND ";
+                    }
+                }
+            }
+        }
+    }
+}
