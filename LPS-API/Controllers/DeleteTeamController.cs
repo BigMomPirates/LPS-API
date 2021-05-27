c58b31619644d6086b939f37a47478b5831d60ec
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-05-27 17:22:28 +0900

    feat: team 삭제 구현

---
+++ LPS-API/Controllers/DeleteTeamController.cs
@@ -0,0 +1,51 @@
+﻿using LPS_API;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace LpsApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeleteTeamController : ControllerBase
+    {
+        private readonly ILogger<DeleteTeamController> _logger;
+
+        public DeleteTeamController(ILogger<DeleteTeamController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public DeleteTeam Get(int id)
+        {
+            string result = "OK";
+
+            try
+            {
+                using MySqlConnection conn = Program.GetMySqlConnection();
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "DELETE FROM team WHERE id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+
+                if (cmd.ExecuteNonQuery() == 0)
+                {
+                    throw new Exception("0 row affected");
+                }
+            }
+            catch (Exception e)
+            {
+                result = e.Message;
+            }
+
+            return new DeleteTeam()
+            {
+                result = result
+            };
+        }
+    }
+}
---
+++ LPS-API/Models/DeleteTeam.cs
@@ -0,0 +1,7 @@
+namespace LpsApi
+{
+    public class DeleteTeam
+    {
+        public string result { get; set; }
+    }
+}
